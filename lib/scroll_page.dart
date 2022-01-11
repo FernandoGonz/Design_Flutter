9b919a5d5026c7ceadcf7268433f57f0da314d89
Fernando <fernando@agovest.com>
2022-01-11 11:28:38 -0600

    Finalizing PageView design

--- lib/scroll_page.dart
+++ lib/scroll_page.dart
@@ -5,12 +5,28 @@ class ScrollPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        scrollDirection: Axis.vertical,
-        children: [
-          Page1(),
-          Page1(),
-        ],
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: [
+              0.5,0.5
+            ],
+            colors: [
+              Color(0xff5EE8C5),
+              Color(0xFF30BAD6),
+            ]
+          ),
+        ),
+        child: PageView(
+          physics: BouncingScrollPhysics(),
+          scrollDirection: Axis.vertical,
+          children: [
+            Page1(),
+            Page2(),
+          ],
+        ),
       ),
     );
   }
@@ -20,11 +36,11 @@ class Page1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Stack(
-        children: [
-          PersonalizedBackground(),
-          MainContent(),
-        ],
-      );
+      children: [
+        PersonalizedBackground(),
+        MainContent(),
+      ],
+    );
   }
 }
 
@@ -57,9 +73,7 @@ class MainContent extends StatelessWidget {
             ),
           ),
           Expanded(
-            child: Container(
-              color: Colors.transparent
-            ),
+            child: Container(color: Colors.transparent),
           ),
           const Icon(
             Icons.keyboard_arrow_down_outlined,
@@ -83,3 +97,20 @@ class PersonalizedBackground extends StatelessWidget {
     );
   }
 }
+
+class Page2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xFF30BAD6),
+      child: Center(
+        child: ElevatedButton(
+          onPressed: () {},
+          child: Text('Bienvenido'),
+        ),
+      ),
+    );
+  }
+}
+
+
