7f6e89a3db1dfcf50e2608e5a95906ef5ece2251
Fernando <fernando@agovest.com>
2022-01-15 11:48:29 -0600

    Adding new screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:dessing_app/home.dart';
 import 'package:dessing_app/scroll_page.dart';
+import 'package:dessing_app/wonderful_home.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -10,10 +11,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      initialRoute: 'scroll_page',
+      initialRoute: 'wonderful_home',
       routes: {
         'home': (BuildContext context) => Home(),
         'scroll_page': (BuildContext context) => ScrollPage(),
+        'wonderful_home': (BuildContext context) => WonderfulHome(),
       },
     );
   }
