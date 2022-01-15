f507aea6b093da48d28e9fea6ff5a1ded121bf83
Fernando <fernando@agovest.com>
2022-01-15 13:35:16 -0600

    ASaving all

--- lib/widgets/personalized_bottombar.dart
+++ lib/widgets/personalized_bottombar.dart
@@ -11,6 +11,7 @@ class PersonalizedBottomBar extends StatelessWidget {
       selectedItemColor: Colors.pink,
       backgroundColor: const Color.fromRGBO(55, 57, 84, 1),
       unselectedItemColor: const Color.fromRGBO(116, 117, 152, 1),
+      currentIndex: 0,
       items: const [
         BottomNavigationBarItem(
           label: 'Calendar',
